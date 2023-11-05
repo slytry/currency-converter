9edb8d3419bcd10299bfcd354ea478ece3f2735d
Nikita Naumov <nanaumov@avito.ru>
2023-11-05 21:55:36 +0300

    chore: fix case in imports

--- src/__tests__/sum.js
+++
@@ -1,5 +0,0 @@
-describe('Name of the group', () => {
-    it('should be equal', () => {
-        expect(1 + 1).toBe(2)
-    });
-});
--- src/componets/quote/Quote.test.jsx
+++ src/componets/quote/Quote.test.jsx
@@ -1,7 +1,7 @@
 
 import { render, screen } from '@testing-library/react'
-import { Quote } from './Quote'
 import { quotes } from './config';
+import { Quote } from './quote';
 
 describe('When quote rendered', () => {
 
